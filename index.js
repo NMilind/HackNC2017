f80e107c05c8463140ad90ab3875330dca8f723e
NMilind <nikhil.milind@hotmail.com>
2017-11-04 20:16:33 -0400

    HTML/CSS Commits

--- index.js
+++ index.js
@@ -41,3 +41,78 @@ $(document).ready(function() {
     heatmap.setMap(map);
   });
 })
+
+
+function createPolygonWithLabel(map, label, points) {
+
+      // The state outline polygon, drawn by coordinates..
+      var polygon = new google.maps.Polygon({
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,
+          map: map,
+          paths: points   ,
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 3
+      });
+
+      // The label that pops up when the mouse moves within each polygon.
+      /*var marker = new MarkerWithLabel({
+          labelContent: label,
+          labelClass: 'label',
+          labelStyle: {
+              opacity: 1.0
+          },
+          position: new google.maps.LatLng(0, 0),  // a LatLng is required
+          draggable: false,
+          raiseOnDrag: false,
+          map: map,
+          icon: 'http://placehold.it/1x1',  // disable marker pin icon
+          visible: false
+      });*/
+
+      // When the mouse moves within the polygon, display the label and change the BG color.
+      google.maps.event.addListener(polygon, "mousemove", function(event) {
+        //marker.setPosition(event.latLng);
+        //marker.setVisible(true);
+        polygon.setOptions({
+            fillColor: "#00FF00"
+        });
+    });
+
+    // WHen the mouse moves out of the polygon, hide the label and change the BG color.
+    google.maps.event.addListener(polygon, "mouseout", function(event) {
+        //marker.setVisible(false);
+        polygon.setOptions({
+            fillColor: "#FF0000"
+        });
+    });
+
+    return polygon;
+}
+
+function initialize() {
+    var mapOptions = {
+            zoom: 4,
+            center: new google.maps.LatLng(23, -101),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        },
+        map = new google.maps.Map(document.getElementById('map'),  mapOptions),
+        j;
+
+    for (var i = 0; i < stateCoords.length; i++) {
+
+        var coords = [],
+            label = stateCoords[i][0],
+            points = stateCoords[i][1];
+
+        for (j = 0; j < points.length; j++) {
+            coords.push(
+                new google.maps.LatLng(points[j][0], points[j][1])
+            )
+        }
+        createPolygonWithLabel(map, label, coords);
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
