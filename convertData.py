ae386d528f645a810c96d7284cc1dbd405042b58
NMilind <nikhil.milind@hotmail.com>
2017-11-04 22:41:43 -0400

    H4ck3d

--- convertData.py
+++ convertData.py
@@ -4,24 +4,24 @@ filename = sys.argv[1]
 outputname = sys.argv[2]
 print("Mining %s" % filename)
 
-# Load latitudes and longitudes for state
-latlongs = {}
-with open("data/latlong.csv", "r") as f:
-    for line in f:
-        spt = str(line).strip().split(",")
-        latlongs[spt[0]] = [ float(spt[1]), float(spt[2]) ]
-
-print("Latitudes and Longitudes Loaded")
-
 # Open CSV file
 output = ""
+vals = {}
+valsLs = []
 with open(filename, "r") as f:
     f.readline()
     for line in f:
         spt = str(line).strip().split(",")
-        output += "new google.maps.LatLng(%s, %s)," % (latlongs[spt[0]][0], latlongs[spt[0]][1]) + "\n"
-        for i in range(int(float(spt[1]))):
-            output += "new google.maps.LatLng(%s, %s)," % (latlongs[spt[0]][0], latlongs[spt[0]][1]) + "\n"
+        vals[spt[0]] = float(spt[1])
+        if (float(spt[1]) != -1.0):
+            valsLs.append(float(spt[1]))
+
+valMin = min(valsLs)
+valMax = max(valsLs)
+for key in vals:
+    output += "coloration[\"" + str(key) + "\"] = " + str(vals[key]) + ";\n"
+output += "coloration[\"Minimum\"] = " + str(valMin) + ";\n"
+output += "coloration[\"Maximum\"] = " + str(valMax) + ";\n"
 
 with open(outputname, "w") as f:
     f.write(output)
